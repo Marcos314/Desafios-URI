6bf6aab2a6497198d44734b1553ef7d2ae184fce
Marcos Wesley <36406574+Marcos314@users.noreply.github.com>
2018-04-14 22:24:18 -0300

    Add files via upload

---
+++ uri1037.c
@@ -0,0 +1,44 @@
+/*Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+
+O símbolo ( representa "maior que". Por exemplo:
+[0,25]  indica valores entre 0 e 25.0000, inclusive eles.
+(25,50] indica valores maiores que 25 Ex: 25.00001 até o valor 50.0000000
+
+Entrada
+O arquivo de entrada contém um número com ponto flutuante qualquer.*/
+
+#include <stdio.h>
+
+int main (){
+
+	float num;
+	scanf("%f", &num);
+
+	if (num >=0 && num<=25){
+
+	printf("Intervalo [0,25]\n");
+
+	}
+
+	else if (num>25.0 && num<=50.0){
+
+		printf("Intervalo (25,50]\n");
+	}
+
+	else if (num>50.0 && num <=75.0){
+
+		printf("Intervalo (50,75]\n");
+	}
+
+	else if (num>75.0 && num<=100.0){
+
+		printf("Intervalo (75,100]\n");
+	}
+
+	else
+
+		printf("Fora de intervalo\n");
+
+
+	return 0;
+}
