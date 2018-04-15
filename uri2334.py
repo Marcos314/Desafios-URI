83d0c1559ed7d1a0c8d2a230d7ff178eb224c2cc
Marcos Wesley <36406574+Marcos314@users.noreply.github.com>
2018-04-14 22:50:32 -0300

    Add files via upload

---
+++ uri2334.py
@@ -0,0 +1,13 @@
+
+P = int(input())
+while 0 <= P <= 10**19:
+    print()
+        voltaram = P -1
+        print(voltaram)
+if P == 0:
+    voltaram = P
+    print(voltaram)
+else:
+    print('')
+P == -1
+print(-1)    
