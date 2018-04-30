2f98323bbe3973f8ed10212406258d7f084dd564
Marcos Wesley <36406574+Marcos314@users.noreply.github.com>
2018-04-30 17:42:58 -0300

    Update uri1059.c

--- uri1059.c
+++ uri1059.c
@@ -1,25 +1,22 @@
+/*Números pares
+Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha
+*/
 # include <stdio.h>
 
 int main(){
 
 	int cont;
 
-for (cont = 2; cont <= 100; cont+=2)
-{
-
-
-	printf("%i\n",cont );
-} 
-
-
-
-
-
-
-
-
-
-
+		for (cont = 2; cont <= 100; cont+=2)
+		{
+		printf("%i\n",cont );
+		} 
 
 	return 0;
 }
